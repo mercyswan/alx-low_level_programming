dee0eb20ec24baf3d5d3d66bbb99aed89d49344f
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-03-12 21:00:36 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
-* main - prints the phrase with proper grammer, but the outcome is a piece of art
+* main - prints the phrase with proper grammer,
+*but the outcome is a piece of art
 *Return: 0 if exited properly, non-zero ptherwise
 */
 int main(void)
