bdae6db2e8547e68f65b33418b3165ed2ed9fce9
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-03-13 19:26:31 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,21 +12,21 @@ int main(void)
 		char ch = 'a';
 
 
-			while (ch <= 'z')
+		while (ch <= 'z')
+
+{
+		if (ch != 'e' && ch != 'q')
 
 					{
-						if (ch != 'e' && ch != 'q')
 
-					{
+		putchar(ch);
+					}
 
-						putchar(ch);
-													}
+		ch++;
 
-						ch++;
+					}
 
-							}
-
-				putchar('\n');
+		putchar('\n');
 
 return (0);
 
