e9268cf9dee2fa9a71d43ae3155bb34d111f0d9f
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-03-13 19:37:30 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
-
- * main - main block
-
- * Description: Print all numbers of base 10, starting from 0.
-
- * Return: 0
-
- */
+* main - main block
+* Description: Print all numbers of base 10, starting from 0.
+* Return: 0
+*/
 
 int main(void)
 
