ca8f190b653f688b48b94b6519e338b6614e8c5d
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-03-13 18:55:11 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,14 +7,10 @@
 
 
 /**
-
- * main - prints the last digit of the random
-
- * number stored in the variable n
-
- * Return: Always 0 (Success)
-
- */
+* main - prints the last digit of the random
+* number stored in the variable n
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 
