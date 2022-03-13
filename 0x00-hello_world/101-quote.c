af3c9175613918dc148a6eb2fce3cdf79f23146d
mercyswan <mercymusa4@gmail.com>
2022-03-13 07:50:10 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <unintd.h>
+#include <unistd.h>
 /**
 *main - prints exactly "and that piece of art is useful"
-*- Dora Korpar, 2015-10-19",
-*followed by a new line, to the stand and error.
-*Return: always 0 (Success)
+* - Dora Korpar, 2015-10-19",
+*followed by a new line, to the standard error.
+*Return: Always 0 (Success)
 */
 int main(void)
 {
