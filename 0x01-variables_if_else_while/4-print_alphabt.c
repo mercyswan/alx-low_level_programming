d8f156c66c80e2ac8054f98b5872f52f4fb9892e
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-03-13 19:22:27 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,29 +4,24 @@
  *
  * main - prints the alphabet in lowercase,
  * followed by a new line, except q and e
- *
  * Return: Aways 0 (Sucess)
  */
 
 int main(void)
 
 {
-
 		char ch = 'a';
-
 
 
 			while (ch <= 'z')
 
 					{
-
-								if (ch != 'e' && ch != 'q')
+							if (ch != 'e' && ch != 'q')
 
 										{
 
 													putchar(ch);
-
-														}
+													}
 
 										ch++;
 
