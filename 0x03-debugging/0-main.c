8bef52fb849da5d6534ef4e57869ce351fec0ee3
mercyswan <mercymusa4@gmail.com>
2022-03-20 06:03:18 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,45 +1,14 @@
 #include "main.h"
-
-
-
 /**
-<<<<<<< HEAD
- *
- *  * main - tests function that prints if integer is positive or negative
- *
- *   * Return: 0
- *
- *    */
-
-
-
-=======
-*main-main
-*Return: 0
+* main - main
+* Return: 0
 */
->>>>>>> cc0a398911e6c72a8475bd0054bf61f8bee07fd0
-int main(void)
-
+int  main(void)
 {
+int i;
 
-		int i;
+i=o;
+positive_or_negative(i);
 
-<<<<<<< HEAD
-
-
-			i = 0;
-
-				positive_or_negative(i);
-
-
-
-					return (0);
-
-=======
-        i = 0;
-        positive_or_negative(i);
-
-
-        return (0);
->>>>>>> cc0a398911e6c72a8475bd0054bf61f8bee07fd0
+return (0);
 }
