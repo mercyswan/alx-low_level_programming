095f085a0a6168382359cd98379205e23c3a5ca7
mercyswan <mercymusa4@gmail.com>
2022-03-22 11:08:07 -0700

    Task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,81 +1,40 @@
 #include "main.h"
 
+
+
 /**
- * print_number - print out a number using printchar.
- * @n: the number you want to print using putchar
- * Return: 0.
- */
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+*/
 
 void print_number(int n)
 
 {
 
-	int  temp, temp2, cnt, div, result, i;
+		unsigned int num = n;
 
 
-	cnt = 0;
 
-	div = 1;
+			if (n < 0)
 
-	if (n < 0)
+					{
 
-	{
+								_putchar('-');
 
-		_putchar('-');
+										num = -num;
 
-	}
+											}
 
-	if (n == 0)
 
-	{
 
-		_putchar('0');
+				if ((num / 10) > 0)
 
-	}
+							print_number(num / 10);
 
-	else
 
-	{
 
-		temp = n;
+					_putchar((num % 10) + '0');
 
-		while (temp)
-
-		{
-
-			temp /= 10;
-
-			++cnt;
-
-		}
-
-		temp2 = cnt;
-
-		while (temp2 > 1)
-
-		{
-			div *= 10;
-
-			--temp2;
-
-		}
-
-		for (i = 0; i < cnt; ++i)
-
-		{
-
-			result = n / div;
-
-			if (result < 0)
-
-				result *= -1;
-
-			_putchar(result + '0');
-
-			n %= div;
-
-			div /= 10;
-
-		}
-	}
 }
+
+© 2022
