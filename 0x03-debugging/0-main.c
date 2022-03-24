b6898a29ccc077297d6778cd3f6222b90778e28a
mercyswan <mercymusa4@gmail.com>
2022-03-24 02:55:44 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,27 @@
-#include "main.h"
+include "main.h"
 
 /**
-* main - main
-* Return: 0
-*/
-int main(void)
-{
-int i;
+ * main - main
+ * Return: 0
+ */
 
-  i = 0;
-positive_or_negative(i);
-  
- return (0);
-  
+
+int main(void)
+
+{
+
+	int i;
+
+	i = 0;
+
+
+
+	positive_or_negative(i);
+
+
+
+	return (0);
+
+
+
 }
