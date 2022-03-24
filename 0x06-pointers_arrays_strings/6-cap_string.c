697728f7ecab8fac900be9081e2b6064c532788c
mercyswan <mercymusa4@gmail.com>
2022-03-24 11:31:00 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,101 +1,51 @@
-#include "main.h
-
+#include "main.h"
 /**
- * separators - checks and ensure that all string is capitalized
- * @c: character to be checked
- * Return: if separator return 1. Otherwise return 0;
- */
-
-int separator(char c)
-
-{
-
-switch (c)
-
-{
-
-case ' ':
-
-case '\t':
-
-case '\n':
-
-case ',':
-
-case ';':
-
-case '.':
-
-case '!':
-
-case '?':
-
-case '"':
-
-case '(':
-
-case ')':
-
-case '{':
-
-case '}':
-
-return (1);
-
-
-
-default:
-
-return (0);
-
-}
-
-
-
-
-
-}
-
-/**
- * cap_string - capitalizes all words of a string
- * @s: string to uppercase
- * Return: returns the modified string
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
+ *Return: pointer to s.
  */
 
 char *cap_string(char *s)
 
 {
 
-int count, upper;
+int count;
 
-
-
-upper = -32;
-
+/*  scan through string */
 
 count = 0;
-
-
 while (s[count] != '\0')
 
-{
+{/* if next character after count is a char , capitalise it */
 
-if (s[count] >= 'a' && s[count] <= 'z')
+if (s[0] >= 97 && s[0] <= 122)
+										{
+										s[0] = s[0] - 32;
+										}
 
-{
+										if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    								|| s[count] == ',' || s[count] == ';' || s[count] == '.'
 
+|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+									
+|| s[count] == '"' || s[count] == '(' || s[count] == ')'
 
+|| s[count] == '{' || s[count] == '}')
 
-if (s[count] == *s || separator(s[count - 1]))
+										{
 
+										if (s[count + 1] >= 97 && s[count + 1] <= 122)
+										{
 
-s[count] += upper;
+										s[count + 1] = s[count + 1] - 32;
 
 }
 
-count++;
-
-}
+										}
+										count++;
+											}
 
 return (s);
 
