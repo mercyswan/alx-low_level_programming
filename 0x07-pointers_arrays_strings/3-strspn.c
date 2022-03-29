1520240df31eca2b0738acb8828af69f0fd6e340
mercyswan <mercymusa4@gmail.com>
2022-03-29 04:35:34 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,69 +7,74 @@
  * Return: return count
  **/
 
+
+
 unsigned int _strspn(char *s, char *accept)
+
+
 
 {
 
-	int i, j;
+		int i, j;
 
-	int count = 0;
+			int count = 0;
 
-	char *str1, *str2;
-
-
-	str1 = s;
-
-	str2 = accept;
+				char *str1, *str2;
 
 
 
-	i = 0;
+					str1 = s;
 
-	while (str1[i] != '\0') /*Declaring WHILE *s */
-
-	{
-
-		j = 0;
-
-		while (str2[j] != '\0') /*Declaring WHILE *accept*/
-
-		{
-
-			if (str2[j] == str1[i]) /*Evaluate condition*/
-
-			{
-
-				count++; /*count number*/
-
-				break;
-
-			}
+						str2 = accept;
 
 
 
-			j++;    /*add j+1*/
+							i = 0;
 
-		}
+								while (str1[i] != '\0') /*Declaring WHILE *s */
+
+										{
+
+													j = 0;
+
+															while (str2[j] != '\0') /*Declaring WHILE *accept*/
+
+																		{
+
+																						if (str2[j] == str1[i]) /*Evaluate condition*/
+
+																										{
+
+																															count++; /*count number*/
+
+																																			break;
+
+																																						}
 
 
 
-		if (s[i] != accept[j]) /*If aren't equals*/
+																									j++;    /*add j+1*/
 
-		{
-
-			break;
-
-		}
+																											}
 
 
 
-		i++; /*add x+1*/
+																	if (s[i] != accept[j]) /*If aren't equals*/
 
-	}
+																				{
+
+																								break;
+
+																										}
 
 
 
-	return (count); /*return the value of count*/
+																			i++; /*add x+1*/
+
+																				}
+
+
+
+									return (count); /*return the value of count*/
 
 }
