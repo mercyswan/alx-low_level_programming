640170255d13fe9b2b4bc3fbf0142559d5aa1d7b
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-03-29 13:01:31 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * _strspn - gets lengthof a prefix substring
  * @s: string to check
@@ -10,33 +8,18 @@
  */
 
 unsigned int _strspn(char *s, char *accept)
-
+	
 {
-
-		unsigned int i, j;
-
-
-
-			for (i = 0; s[i]; i++)
-
-					{
-
-								for (j = 0; accept[j]; j++)
-
-											{
-
-															if (s[i] == accept[j])
-
-																				break;
-
-																	}
-
-										if (!accept[j])
-
-														break;
-
-											}
-
-				return (i);
-
+unsigned int i, j;
+for (i = 0; s[i]; i++)
+{
+for (j = 0; accept[j]; j++)
+{
+if (s[i] == accept[j])
+break;
+}
+if (!accept[j])
+break;
+}
+return (i);
 }
