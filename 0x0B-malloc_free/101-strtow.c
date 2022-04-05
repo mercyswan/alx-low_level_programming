d7e09628cdb78ffb21e8899a1b15581648dc3dfc
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-04-05 17:18:05 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,42 +13,31 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-		char *dest, *src;
-			unsigned int i;
-
-				if (new_size == old_size)
-							return (ptr);
-
-					if (ptr == NULL)
-
-}
-						ptr = malloc(new_size);
-						if (ptr == NULL)
+char *dest, *src;
+unsigned int i;
+if (new_size == old_size)
+return (ptr);
+if (ptr == NULL)
 {
-						return (NULL);
-}
+ptr = malloc(new_size);
+if (ptr == NULL)
 {
+return (NULL);
+}
 return (ptr);
 }
-
-						if (new_size == 0 && ptr != NULL)
+if (new_size == 0 && ptr != NULL)
 {
-							free(ptr);
-							return (NULL);
+free(ptr)
+return (NULL);
 }
+dest = malloc(new_size);
+if (dest == NULL)
+return (NULL);
+src = ptr;
+for (i = 0; i < new_size && i < old_size; i++)
+dest[i] = src[i];
+free(ptr);
 
-							dest = malloc(new_size);
-								if (dest == NULL)
-								return (NULL);
-
-									src = ptr;
-
-										for (i = 0; i < new_size && i < old_size; i++)
-
-										dest[i] = src[i];
-											free(ptr);
-
-
-											return (dest);
-
+return (dest);
 }
