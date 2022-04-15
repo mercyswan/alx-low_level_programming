0a7bffdead3189d47e9f642d3a6a5b330dfaa39a
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-04-15 02:10:09 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_function/0-sum_them_all.c
+++ 0x10-variadic_function/0-sum_them_all.c
@@ -1,8 +1,8 @@
-#include "variadic_functions.h
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - returns the sum of all its parameters.
- * @n: amount of the argumen. *
+ * @n: amount of the argumen.
  * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
