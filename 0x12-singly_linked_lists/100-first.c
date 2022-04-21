ecd4a3a09c89ee98a45335d75351c12a98e9b1c1
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-04-22 00:44:55 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * bmain - function executed before main
  * Return: no return.
  */
-
 void __attribute__((constructor)) bmain()
 {
     printf("You're beat! and yet, you must allow");
