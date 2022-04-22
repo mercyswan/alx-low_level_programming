5098ead2b0ef49c8ffbc37e0037427e463891bcb
Mercy Swanta <99684074+mercyswan@users.noreply.github.com>
2022-04-22 02:19:50 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,10 @@ size_t nbr_nodes = 0;
 const list_t *current = h;
 while (current != NULL)
 {
-  printf("[%d] %s\n", current->len,
-  current->str != NULL ? current->str : "(nil)");
-  current = current->next;
-  nbr_nodes++;
+printf("[%d] %s\n", current->len,
+current->str != NULL ? current->str : "(nil)");
+current = current->next;
+nbr_nodes++;
 }
 return (nbr_nodes);
 }
